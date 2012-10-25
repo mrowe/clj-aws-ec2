6a698b1d00e93706cdce2f42deda1db8998c5bc0
Michael Rowe <mrowe@mojain.com>
2012-10-25 16:02:30 +1100

    Add source url and licence details

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject clj-aws-ec2 "0.1.1"
   :description "Clojure Amazon EC2 library."
+  :url "https://github.com/mrowe/clj-aws-ec2"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [com.amazonaws/aws-java-sdk "1.3.18"]]
   :plugins [[codox "0.6.1"]])
