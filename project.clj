7dc36ea7fe5733835951f5057f098da1b52ab194
Michael Rowe <mrowe@mojain.com>
2012-10-26 13:59:48 +1100

    Bump version (new functionality).

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-aws-ec2 "0.1.2"
+(defproject clj-aws-ec2 "0.1.3"
   :description "Clojure Amazon EC2 library."
   :url "https://github.com/mrowe/clj-aws-ec2"
   :license {:name "Eclipse Public License"
