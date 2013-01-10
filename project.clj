f99b68ed4eb4895bcd9bec848b3aa2ac38262365
Michael Rowe <mrowe@mojain.com>
2013-01-10 17:42:54 +1100

    Start work on 0.1.10

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-aws-ec2 "0.1.9"
+(defproject clj-aws-ec2 "0.1.10"
   :description "Clojure Amazon EC2 library."
   :url "https://github.com/mrowe/clj-aws-ec2"
   :license {:name "Eclipse Public License"
