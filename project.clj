ca123ecd01183e36bf11e26eb7f66a8dc4dd5019
Michael Rowe <mrowe@mojain.com>
2013-01-10 17:43:57 +1100

    Upgrade to latest AWS SDK

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.3.18"]]
+                 [com.amazonaws/aws-java-sdk "1.3.23"]]
   :plugins [[codox "0.6.1"]])
