c6505ec92decfcbf4b333ebbabf45c02f37b9dd7
Michael Rowe <mrowe@mojain.com>
2013-04-15 17:24:51 +1000

    Upgrade to v1.4.3 of the AWS Java SDK

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.3.23"]]
+                 [com.amazonaws/aws-java-sdk "1.4.3"]]
   :plugins [[codox "0.6.1"]])
